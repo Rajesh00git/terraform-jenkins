0d1ca34b57972335ba35adf3f00e77b4fd9670e2
Rajesh00git <136963194+Rajesh00git@users.noreply.github.com>
2024-01-15 18:51:15 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "rajesh-hyd-test"
+    bucket = "s3-hyd-buck"
     key = "terraform.tfstate"
     region = "us-east-1"
     #dynamodb_table = "DynamoDbLock"
