5ad3579439712e47ed0926efa48e8d7ffbd0c20e
Rajesh00git <136963194+Rajesh00git@users.noreply.github.com>
2024-01-15 17:52:17 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,10 +3,7 @@ variable "aws_region" {
        default     = "us-east-1" 
 }
 
-variable "key_name" { 
-    description = " SSH keys to connect to ec2 instance" 
-    default     =  "test" 
-}
+
 
 variable "instance_type" { 
     description = "instance type for ec2" 
